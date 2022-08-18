e3b2a26ae0c95abf3e6e8d52a68f5aa030aa4429
Cristian Planas <me@cristianplanas.com>
2022-08-17 20:23:49 -0700

    Added basic Film model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_032242) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_032334) do
+  create_table "films", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
