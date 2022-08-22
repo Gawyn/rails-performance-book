06a244e8bb168bd89cd9ce0ae5df52077aa187bb
Cristian Planas <me@cristianplanas.com>
2022-08-21 18:12:01 -0700

    Schema adapted to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_19_212759) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "films", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
