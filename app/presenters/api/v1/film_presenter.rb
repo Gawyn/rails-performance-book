d761c23b7aa6904a10f7cd7d5e28ce5b9063447f
Cristian Planas <me@cristianplanas.com>
2023-01-21 11:13:13 -0500

    Removing caching system

--- app/presenters/api/v1/film_presenter.rb
+++ app/presenters/api/v1/film_presenter.rb
@@ -6,27 +6,6 @@ class Api::V1::FilmPresenter
   end
 
   def to_json
-    cached_object
-  end
-
-  private
-
-  def cached_object
-    object = Rails.cache.fetch(cache_key)
-
-    return object.tap { |h| h.delete(:expiring_key) } if object && object[:expiring_key] = resource.updated_at
-
-    jsonize.merge(expiring_key: resource.updated_at).tap do |object|
-      Rails.cache.write(cache_key, object)
-    )
-    end
-  end
-
-  def cache_key
-    "Film-#{resource.id}"
-  end
-
-  def jsonize
     {
       id: resource.id,
       title: resource.title
