d9f632172e5bc2da696dc606d0ca64ebfec78541
Cristian Planas <me@cristianplanas.com>
2023-02-18 01:22:37 -0500

    Setting up cached timeline

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :followeds, through: :followings
+
+  def timeline_cache_key
+    @timeline_cache_key ||= "rental-timeline-##{id}"
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,17 @@
 class Rental < ApplicationRecord
+  after_create :cache_for_followers
   belongs_to :customer
   belongs_to :inventory
   has_one :store, through: :inventory
   has_one :film, through: :inventory
+
+  private
+
+  def cache_for_followers
+    customer.followers.each do |follower|
+      timeline = Rails.cache.fetch(follower.timeline_cache_key) || []
+
+      Rails.cache.write(follower.timeline_cache_key, timeline.append(id))
+    end
+  end
 end
