a081bb6c0bb8df16ade9f48268057431c2c1034c
Cristian Planas <me@cristianplanas.com>
2023-07-06 11:34:57 +0200

    Inventory product limits

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,16 @@
 class Inventory < ApplicationRecord
+  MAX_AMOUNT_PER_STORE = 10000
+
   belongs_to :film
   belongs_to :store
+
+  validate :max_amount_of_inventory_per_store
+
+  private
+
+  def max_amount_of_films_per_store
+    if store.inventories.count >= MAX_AMOUNT_PER_STORE
+      errors.add(:store, 'Store has the max amount of items in the inventory.')
+    end
+  end
 end
