792f27804ac5cddfaffc04f580ff22667d8a0f06
Cristian Planas <me@cristianplanas.com>
2025-02-12 22:36:54 +0100

    Reducing CustomerPresenter

--- app/presenters/api/v1/customer_presenter.rb
+++ app/presenters/api/v1/customer_presenter.rb
@@ -8,8 +8,7 @@ class Api::V1::CustomerPresenter
   def to_json
     {
       id: resource.id,
-      name: resource.name,
-      rental_counter: resource.rentals.count
+      name: resource.name
     }
   end
 end
