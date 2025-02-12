06f8f66d0b777603819e1e2254fdd1f2a38568e5
Cristian Planas <me@cristianplanas.com>
2025-02-12 03:47:15 +0100

    most_rented_film is a method on the baseline branch

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,7 +3,10 @@ class Store < ApplicationRecord
   has_many :films, through: :inventories
   belongs_to :most_rented_film, class_name: 'Film'
 
-  def set_most_rented_film!
-    update_attribute(:most_rented_film_id, Rental.joins(:inventory).where(inventory: {store_id: id}).group(:film_id).count.max_by { |k, v| v }.last)
+
+  def most_rented_film
+    Film.find(Rental.joins(:inventory)
+      .where(inventory: {store_id: id})
+      .group(:film_id).count.max_by { |k, v| v }.last)
   end
 end
