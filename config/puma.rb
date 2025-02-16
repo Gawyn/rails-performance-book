7249032b5929366d4818fe9f1e58a7f24b36cf30
Cristian Planas <me@cristianplanas.com>
2025-02-16 19:45:35 +0100

    Configuring Solid Queue

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# You can either set the env var, or check for development
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || Rails.env.development?
