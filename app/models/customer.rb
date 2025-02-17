3a821a33f9b70e9c69cc9e8ef730979f0be75783
Cristian Planas <me@cristianplanas.com>
2025-02-17 11:24:30 +0100

    Adding follower association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,7 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :followeds, through: :followings
+
+  has_many :inverse_followings, foreign_key: 'followed_id', dependent: :destroy, class_name: 'Following'
+  has_many :followers, through: :inverse_followings
 end
